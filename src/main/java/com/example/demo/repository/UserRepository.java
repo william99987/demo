83abe9bb5266cf7e157387a51b7c51bb030580f2
Weiliang Huang <whua0055@student.monash.edu>
2025-01-15 09:44:32 +0800

    Change repository annotation to redisrepo annotation

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,12 +1,14 @@
 package com.example.demo.repository;
 
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+@RedisHash("User")
+public interface UserRepository extends CrudRepository<User, Long>{
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
 }
